cb21d764f4522f063cbe9a4edffe47679a1c39da
Josh Cheek <josh.cheek@gmail.com>
2016-06-28 10:47:39 -0500

    Some quines

---
+++ quines/harbor_jubilee.rb
@@ -0,0 +1,38 @@
+
+                                  __=               __
+                      class        <<             __
+                                    __=         __
+                            class    <<       __
+                                 def  <<    __
+                                      $_= __
+                                    print
+                                 .tap { puts }
+                             end     or   ___
+                         end        or     ____
+                     end           or       _____
+                                 def         << __=
+                               (_=_)
+                                               _<<__
+                            end and->                 \
+                           _=:i,i:_              {__<<_}
+.(
+(i="%1$s\n.(\n(i=%3$p) %% [l=\n\"%2$s\",\nl.gsub(\"\\\\\", \"\\\\\\\\\\\\\"),\ni])") % [l=
+"
+                                  __=               __
+                      class        <<             __
+                                    __=         __
+                            class    <<       __
+                                 def  <<    __
+                                      $_= __
+                                    print
+                                 .tap { puts }
+                             end     or   ___
+                         end        or     ____
+                     end           or       _____
+                                 def         << __=
+                               (_=_)
+                                               _<<__
+                            end and->                 \\
+                           _=:i,i:_              {__<<_}",
+l.gsub("\\", "\\\\\\"),
+i])
