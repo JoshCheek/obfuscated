78b62f1f813ae07a6e23ed06d8f2c3166c1975f9
Josh Cheek <josh.cheek@gmail.com>
2018-11-01 22:36:58 -0500

    The roof is on fire

---
+++ the_roof_is_on_fire.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby -W0
+
+public def !
+  gsub /#$/| +/,%/ /
+end
+
+class Symbol
+  else
+  def
+
+  <<* w,t:?f
+  yield end.
+  << do raise +$/+
+
+  ! <<-"the roof."
+        the roof
+        the roof.
+end
+
+# just to be cool:
+end rescue begin
+
+# just to be cruel:
+$><<%!#$!!*-~-~$.<<!!%!
+
+is
+on
+fire!
+
+# fin!
+puts end
+
+# >>
+# >>  the roof
+# >>  the roof
+# >>  the roof  is on fire
